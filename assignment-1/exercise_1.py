736eb03d474f9d928fd358499f35dc3afbe466fb
İrem Gündüz <irembgunduz@gmail.com>
2022-11-09 15:19:48 +0100

    exercise 1,2

---
+++ assignment-1/exercise_1.py
@@ -0,0 +1,25 @@
+from exercise_2 import randomMatrixGenerator
+import random
+
+#create random matrix 1
+np.random.seed(5)
+mat1 = randomMatrixGenerator(4,4)
+
+#create random matrix 2
+np.random.seed(8)
+mat2= randomMatrixGenerator(4,4)
+
+#create random matrix 3
+np.random.seed(18)
+mat3= randomMatrixGenerator(4,4)
+
+def check(mat1,mat2,mat3):
+    res1 = (mat1 * mat2) * mat3
+    res2 = mat1 * (mat2 * mat3)
+    if res1.any() != res2.any():
+        print("(mat1 * mat2) * mat3 is NOT equal to mat1 * (mat2 * mat3)")
+    else:
+        print("(mat1 * mat2) * mat3 is equal to mat1 * (mat2 * mat3)")
+
+#check if the equation is TRUE
+check(mat1,mat2,mat3)
---
+++ assignment-1/exercise_2.py
@@ -0,0 +1,35 @@
+from numpy import linalg as ln
+import numpy as np
+import random
+
+def randomMatrixGenerator(a,b):
+  """Randomly generate AxB matrix from the integers between 0-10"""
+  mat = np.random.randint(low=1, high=9, size=(a,b))
+  return mat
+
+def findInverseTransMatrix(a,b):
+    """Function that does the following tasks:
+    1.Randomly generate axb matrix
+    2.Transpose the matrix
+    3.Find the inverse of the transposed matrix"""  
+    mat = randomMatrixGenerator(a,b)
+    print("Original matrix is ", mat,"\n")
+    tmat = mat.transpose()
+    print("Transpose of the original matrix is ", tmat,"\n")
+    itmat = np.linalg.inv(tmat)
+    print("Inverse of the transposed matrix is ", itmat,"\n")
+    return itmat
+  
+def computeEigens(a,b):
+  """Function that compute eigenvalues and eigenvectors of a randomly generated matrix"""
+  mat = randomMatrixGenerator(a,b)
+  evalue,evector = ln.eig(mat)
+  return evalue,evector
+
+#Exercise 2, question 1
+np.random.seed(18)
+findInverseTransMatrix(4,4)
+
+#Exercise 2, question 2
+np.random.seed(21)
+computeEigens(a,b)
