fb007608c8a847a8d4ddccdb1c0503c2070b2bf6
Tuan <anhtuanhsgs@gmail.com>
2023-01-24 12:42:34 +0100

    updated submitted solution for assignment 9

--- assignment-9/solution.py
+++ assignment-9/solution.py
@@ -8,66 +8,83 @@ import torch.nn.functional as F
 
 
 
+class BlockCNN(Module):
+    def __init__(self,in_channels, out_channels, kernel_size):
+        super(BlockCNN, self).__init__()
+        self.conv_1 = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size//2)
+        self.conv_2 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=kernel_size//2)
+        self.conv_3 = nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=kernel_size//2)
+        self.relu = nn.ReLU()
+    
+    def forward(self, x):
+        inp = x
+        x1 = self.relu(self.conv_1(x))
+        x2 = self.relu(self.conv_2(x1))
+        x3 = self.relu(self.conv_3(x2))
+        x_out = x1 + x3
+        return x_out
 
+		
 
 class CNN_model(Module):
     
-    def __init__(self, in_channels= 3, out_channels = 32,  
-                 kernel_size = 3, stride = 1, padding = 1, num_classes = 10):
+    def __init__(self, 
+    in_channels=3, 
+    num_blocks=3, 
+    kernel_size=3, 
+    filter_size=32, 
+    num_classes=10,
+    pool_size=4):
         
         super(CNN_model, self).__init__()
+        self.num_blocks = num_blocks
+        blocks = []
 
-        self.conv_layer1 = Conv2d(in_channels, out_channels, kernel_size, stride, padding)
-        self.max_pool1 = MaxPool2d(kernel_size = kernel_size, stride = stride)
-        self.relu = ReLU()
+        for i in range (num_blocks):
+            if i == 0:
+                _in_channels = in_channels
+            else:
+                _in_channels = filter_size
+            blocks.append(BlockCNN(_in_channels, filter_size, kernel_size=kernel_size))
+        
+        self.cnn_blocks = nn.Sequential(*blocks)
+        self.pool = nn.MaxPool2d(kernel_size=pool_size, stride=pool_size)
 
-        self.conv_layer2 = Conv2d(in_channels=32, out_channels= 32, kernel_size = 3, stride = 2, padding = 0)
-        self.max_pool2 = MaxPool2d(kernel_size = kernel_size, stride = stride)
-    
-        self.fc1 = nn.Linear(in_features= 32 * 12*12, out_features=1024)
-        self.relu2 = nn.ReLU()
+        self.fc1 = nn.Linear(in_features= filter_size*((32 // pool_size)**2), out_features=1024)
+        self.relu = nn.ReLU()
         self.fc2 = nn.Linear(1024, 512)
         self.fc3 = nn.Linear(512, num_classes)
        
 
 
     def forward(self, x):
-        x = self.conv_layer1(x)
-        x= self.relu(x)
-        x= self.max_pool1(x)
-       # print("Output shape after maxpool1:", x.shape)
-        x = self.conv_layer2(x)
-        x= self.max_pool2(x)
-       # print("Output shape after maxpool2:", x.shape)
-#         x = nn.Flatten(x)
-#         print("shape aftesr flattening: ", x.size)
-        x = x.view(-1, 32 * 12*12)
-        x = self.fc1(x)
-        
-        x = self.relu2(x)
-        x = self.fc2(x)
-        x = self.relu2(x)
+        x = self.cnn_blocks(x)
+        x = self.pool(x)
+        x = x.view(x.shape[0],-1)
+        x = self.relu(self.fc1(x))
+        x = self.relu(self.fc2(x))
         x = self.fc3(x)
-        x = softmax(x)
-#         outputs = torch.argmax(x, dim=3)
         return x
-    
-    
-    
 
 
-def load_data_svhn(split):
+def load_data_svhn(split, batch_size=32):
     
     # Preprocess the data by resizing, center cropping, and normalization based on the data split
-    transform_train = transforms.Compose([transforms.Resize(32),
-                                transforms.CenterCrop(32),
+    transform_train = transforms.Compose([
+                                # transforms.Resize(32),
+                                # transforms.CenterCrop(32),
+                                transforms.Grayscale(1),
                                 transforms.ToTensor(),
-                                transforms.Normalize([0.5, 0.5, 0.5],
-                                                     [0.5, 0.5, 0.5])])
+                                # transforms.Normalize([0.5, 0.5, 0.5],
+                                                    #  [0.5, 0.5, 0.5])
+                                                     ])
     
-    transform_test = transforms.Compose([transforms.ToTensor(),
-                                        transforms.Normalize((0.5, 0.5, 0.5),
-                                                             (0.5, 0.5, 0.5))])
+    transform_test = transforms.Compose([
+                                        transforms.Grayscale(1),
+                                        transforms.ToTensor(),
+                                        # transforms.Normalize((0.5, 0.5, 0.5),
+                                                            #  (0.5, 0.5, 0.5))
+                                                             ])
    # download the SVHN dataset
     if split == 'train':
         svhn_data = datasets.SVHN(root='path/to/data', split='train', download=True, transform=transform_train)
@@ -77,11 +94,20 @@ def load_data_svhn(split):
     else:
         svhn_data = datasets.SVHN(root='path/to/data', split='extra', download=True, transform=transform_test)
     # load the data 
-    data_loader = torch.utils.data.DataLoader(svhn_data, batch_size=32, shuffle=True, num_workers=4, drop_last = True)
+    data_loader = torch.utils.data.DataLoader(svhn_data, batch_size=batch_size, shuffle=False, num_workers=4, drop_last = False)
 
     return data_loader
 
-
+if __name__ == "__main__":
+    model = CNN_model(in_channels=3, 
+                    num_blocks=3, 
+                    kernel_size=3, 
+                    filter_size=32, 
+                    num_classes=10,
+                    pool_size=4)
+    inp = torch.zeros (10, 3, 32, 32)
+    out = model(inp)
+    print (out.shape)
 
     
 
